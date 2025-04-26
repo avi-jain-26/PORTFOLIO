9e7c140f0d7e80265c66eaf5418a1c4847a7db1d
Avi Jain <avijj1726@gmail.com>
2025-04-26 22:42:35 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "web design", "web development"],
+    strings: ["frontend development", "web design", "web development","UI/UX design"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
